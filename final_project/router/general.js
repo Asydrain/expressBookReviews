d42bfc31511123abefe45c8f2e526b1a581fdf00
asydrain <sydevanfrandsen@gmail.com>
2024-03-16 21:56:26 -0400

    completed all tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -6,43 +6,77 @@ const public_users = express.Router();
 
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+        const username = req.body.username;
+        const password = req.body.password;
+      
+        if (username && password) {
+          if (!isValid(username)) { 
+            users.push({"username":username,"password":password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+          } else {
+            return res.status(404).json({message: "User already exists!"});    
+          }
+        } 
+        return res.status(404).json({message: "Unable to register user."});
 });
 
-// Get the book list available in the shop
+// TASK 1 Get the book list available in the shop
 public_users.get('/',function (req, res) {
-    res.send(JSON.stringify({books},null,4));
+    let getAllBooks = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(res.send(JSON.stringify({books},null,4)));
+        });
+    });
 });
 
-// Get book details based on ISBN
+// TASK 2 Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
     const isbn = req.params.isbn;
-    res.send(books[isbn])
+    let getBooksByIsbn = new Promise((resolve,reject) => {
+        setTimeout(() => {
+            resolve(
+                res.send(books[isbn])
+            );
+        });
+    });
  });
   
-// START HERE TASK 3 Get book details based on author
+// TASK 3 Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const keys = Object.keys(books);
     const author = req.params.author;
     let filtered_keys = keys.filter((key) => books[key].author === author);
     if (filtered_keys.length > 0) {
-        res.send(filtered_keys.map(key => books[key]));
-    } else {
-        res.send("No books by that author found.")
+        let getBooksByAuthor = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve(
+                    res.send(filtered_keys.map(key => books[key]))
+                );
+                reject(
+                    res.send("No books by that author found.")
+                );
+            });
+        });
     }
 });
 
-// Get all books based on title
+// TASK 4 Get all books based on title
 public_users.get('/title/:title',function (req, res) {
     const keys = Object.keys(books);
     const title = req.params.title;
     let filtered_keys = keys.filter((key) => books[key].title === title);
     if (filtered_keys.length > 0) {
-        res.send(filtered_keys.map(key => books[key]));
-    } else {
-        res.send("No books by that title found.")
-    }
+        let getBooksByTitle = new Promise((resolve,reject) => {
+            setTimeout(() => {
+                resolve(
+                    res.send(filtered_keys.map(key => books[key]))
+                );
+                reject(
+                    res.send("No books by that title found.")
+                );
+            });
+        });            
+    }  
 });
 
 //  Get book review
